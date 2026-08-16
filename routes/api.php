2a4e71295a38a8a563250721c65af1029bccbb16
Shehab Altawel <shehabltawel@gmail.com>
2026-08-16 05:58:23 +0300

    add throttle, clean up api routes

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,16 @@
 use App\Http\Controllers\Api\V1\Auth\AuthController;
 use App\Http\Controllers\Api\V1\GetAvailableTripSeatsController;
 use App\Http\Controllers\Api\V1\StoreTripBookingController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::post('auth/register', [AuthController::class, 'register']);
-    Route::post('auth/login', [AuthController::class, 'login']);
-    Route::get('trips/available-seats', GetAvailableTripSeatsController::class)->middleware('auth:sanctum');
-    Route::post('trips/{trip}/book-seats', StoreTripBookingController::class)->middleware('auth:sanctum');
-});
+    Route::prefix('auth')->middleware('throttle:6,1')->group(function () {
+        Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class, 'login']);
+    });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+    Route::prefix('trips')->middleware('auth:sanctum')->group(function () {
+        Route::get('available-seats', GetAvailableTripSeatsController::class);
+        Route::post('{trip}/book-seats', StoreTripBookingController::class);
+    });
+});
